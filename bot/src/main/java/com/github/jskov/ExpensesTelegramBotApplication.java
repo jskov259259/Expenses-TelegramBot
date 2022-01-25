5264b374b4320933e9ab6293c1411fb575a73e09
Skov <jskov259259@rambler.ru>
2022-01-25 18:29:41 +0300

    add bot module, ExpensesTelegramBot class

--- bot/src/main/java/com/github/jskov/ExpensesTelegramBotApplication.java
+++ bot/src/main/java/com/github/jskov/ExpensesTelegramBotApplication.java
@@ -1,4 +1,4 @@
-package com.github.jskov.telegrambot;
+package com.github.jskov;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ExpensesTelegramBotApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExpensesTelegramBotApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ExpensesTelegramBotApplication.class, args);
+    }
 
 }
---
+++ bot/src/main/java/com/github/jskov/bot/ExpensesTelegramBot.java
@@ -0,0 +1,25 @@
+package com.github.jskov.bot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class ExpensesTelegramBot extends TelegramLongPollingBot {
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+
+}
