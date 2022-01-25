f524f793a85db6b73e3d2ffd2e38b8f4dc6eb819
Skov <jskov259259@rambler.ru>
2022-01-25 18:03:28 +0300

    SpringBoot initialization

---
+++ src/main/java/com/github/jskov/telegrambot/ExpensesTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.jskov.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jskov/telegrambot/ExpensesTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jskov.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
