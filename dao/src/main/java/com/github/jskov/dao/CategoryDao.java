77ae8c7e3991f27a959ea465606db04ec8836cb6
Skov <jskov259259@rambler.ru>
2022-01-29 11:41:22 +0300

    working CodeList command

--- dao/src/main/java/com/github/jskov/dao/CategoryDao.java
+++ dao/src/main/java/com/github/jskov/dao/CategoryDao.java
@@ -1,4 +1,17 @@
 package com.github.jskov.dao;
 
+import com.github.jskov.model.Category;
+
+import java.util.List;
+
+/**
+ * Category DAO Interface.
+ */
 public interface CategoryDao {
+
+    /**
+     * Get all categories
+     * @return Category list
+     */
+    List<Category> findAllCategories();
 }
--- service-command/src/main/java/com/github/jskov/command/CodeCommand.java
+++ service-command/src/main/java/com/github/jskov/command/CodeCommand.java
@@ -1,16 +1,35 @@
 package com.github.jskov.command;
 
+import com.github.jskov.dao.CategoryDao;
+import com.github.jskov.model.Category;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.List;
 
 @Service
 public class CodeCommand implements Command {
 
-    public static final String CODE_MESSAGE = "Code list message";
+    public static final String CODE_MESSAGE = "Code list:\n";
 
+    private CategoryDao categoryDao;
+
+    public CodeCommand(CategoryDao categoryDao) {
+        this.categoryDao = categoryDao;
+    }
 
     @Override
     public String execute(Update update) {
-        return CODE_MESSAGE;
+        List<Category> categoryList = findAllCategories();
+        StringBuilder responseMessage = new StringBuilder(CODE_MESSAGE);
+        for (Category category : categoryList) {
+            responseMessage.append(category.getCategoryId() + " " + category.getCategoryName() + "\n");
+        }
+        // TODO make responseMessage from codeList with stream API
+        return responseMessage.toString();
+    }
+
+    private List<Category> findAllCategories() {
+        return categoryDao.findAllCategories();
     }
 }
