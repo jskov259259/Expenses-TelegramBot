04664eb741a1568c02be1b6e6156a3c3cd07990d
Skov <jskov259259@rambler.ru>
2022-01-29 22:51:47 +0300

    change Help command

--- service-command/src/main/java/com/github/jskov/command/HelpCommand.java
+++ service-command/src/main/java/com/github/jskov/command/HelpCommand.java
@@ -6,7 +6,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 @Service
 public class HelpCommand implements Command {
 
-        public static final String HELP_MESSAGE = String.format("I can help you add expenses and get statistics\n" +
+        public static final String HELP_MESSAGE = "I can help you add expenses and get statistics\n" +
                 "<b>Use following commands:</b>\n" +
                 "/code - get category code\n" +
                 "/add - [code] [price] - add expense information. Use delimiter '.'\n\n" +
@@ -14,7 +14,7 @@ public class HelpCommand implements Command {
                 "/get dd.mm.yyyy - get information for the chosen day\n" +
                 "/get dd.mm.yyyy-dd.mm.yyyy - get information for the chosen period\n" +
                 "/get [Period world] - get information for the specific period\n" +
-                "Period worlds: DAY, WEEK, MONTH, YEAR, ALL");
+                "Period worlds: DAY, WEEK, MONTH, YEAR, ALL";
 
 
         @Override
